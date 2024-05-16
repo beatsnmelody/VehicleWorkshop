87ed86e0cb1b033318f264892df5c3bb62904af0
Alexandra Labaisse <beatsnmelody1@gmail.com>
2024-05-16 15:51:55 -0500

    Finished logic for SalesContract and Contract classes

---
+++ src/main/java/org/contract/LeaseContract.java
@@ -0,0 +1,4 @@
+package org.contract;
+
+public class LeaseContract {
+}
---
+++ src/main/java/org/contract/SalesContract.java
@@ -0,0 +1,92 @@
+package org.contract;
+
+import org.example.Vehicle;
+
+public class SalesContract extends Contract {
+
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean isFinanced;
+
+    public SalesContract(String dateOfContract, String customerName, String customerEmail, Vehicle vehicleSold, double salesTaxAmount, double recordingFee, double processingFee, boolean isFinanced) {
+        super(dateOfContract, customerName, customerEmail, vehicleSold);
+        this.salesTaxAmount = salesTaxAmount;
+        this.recordingFee = recordingFee;
+        this.processingFee = processingFee;
+        this.isFinanced = isFinanced;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(double processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public boolean isFinanced() {
+        return isFinanced;
+    }
+
+    public void setFinanced(boolean financed) {
+        isFinanced = financed;
+    }
+
+    @Override
+    public double getTotalPrice() {
+
+        this.salesTaxAmount = getVehicleSold().getPrice() * 1.05;
+        this.recordingFee = 100;
+        this.processingFee = 0;
+
+        if (getVehicleSold().getPrice() < 10000) {
+            this.processingFee = 295;
+        }else{
+            this.processingFee = 495;
+        }
+
+        double totalPrice = salesTaxAmount + recordingFee + processingFee;
+
+        return totalPrice;
+
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+
+        double monthlyPayment = 0;
+
+        if (this.isFinanced && getVehicleSold().getPrice() >= 10000) {
+
+            double totalInterest = getVehicleSold().getPrice() * 4.25;
+
+            monthlyPayment = getVehicleSold().getPrice() + totalInterest / 48;
+
+        }else if (this.isFinanced && getVehicleSold().getPrice() < 10000){
+
+            double totalInterest = getVehicleSold().getPrice() * 5.25;
+
+            monthlyPayment = getVehicleSold().getPrice() + totalInterest / 24;
+
+        }
+
+        return monthlyPayment;
+    }
+}
